7d0b66274e68cf7651a7f7494188414db8696ee3
maradC <117858924+maradC@users.noreply.github.com>
2023-09-18 16:10:39 -0400

    Car.java

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Car {
+
+public class Car {
+
+    String make = "Honda";
+    String model= "Accord";
+    int year = 2022;
+    String color=  "Blue";
+    double price = 40000.00;
+
+    public Car(String make,int year, String color, double price) {
+        this.make = make;
+        this.year = year;
+        this.color = color;
+        this.price = price;
+
+    }
+    public Car (Car a){
+        this.make = a.make;
+        this.year = a.year;
+        this.color = a.color;
+        this.price = a.price;
+    }
+
+        void drive() {
+            System.out.println("You drive the car");
+        }
+        void brake(){
+          System.out.println("You stepped on the break");
+        }
+
+        }
+
 }
