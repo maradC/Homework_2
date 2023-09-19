6474550639ec2feb161a60038b4bcd755d54e6d9
maradC <117858924+maradC@users.noreply.github.com>
2023-09-19 12:40:57 -0400

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,31 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main( String[] args){
+
+    //Objects
         Car myCar1 = new Car("Honda", 2022, "Blue", 40000.00);
-        Car myCar2 = myCar1;
-        System.out.println(myCar1);
-        System.out.println(myCar2);
+        Car myCar2 = new Car("BMW", 2012, "Black", 70000.00);
+        Car myCar3 = new Car("Hyundai", 2019, "Yellow", 38000.00);
+        Car myCar4 = new Car("Tesla", 2023, "White", 100000.00);
+
+
+            //Shallow Copy
+            Car myCar5 = myCar1;
+
+
+    System.out.println(myCar1.make);
+    System.out.println(myCar2.year);
+    System.out.println();
+    System.out.println(myCar3.price);
+    System.out.println(myCar4.model);
         System.out.println();
-        System.out.println(myCar1.color);
-        System.out.println(myCar2.color);
+    System.out.println(myCar1.color);
+    System.out.println(myCar5.color);
+
+        }
+
+
+
+
+
     }
+
