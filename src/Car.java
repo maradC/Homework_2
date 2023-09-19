bb94995900c138a09fda5d1ce39f1df2155745a9
maradC <117858924+maradC@users.noreply.github.com>
2023-09-19 12:41:45 -0400

    Car.java

--- src/Car.java
+++ src/Car.java
@@ -1,27 +1,31 @@
-
 public class Car {
-
     String make = "Honda";
     String model= "Accord";
     int year = 2022;
     String color=  "Blue";
     double price = 40000.00;
 
+    //Default and Parametric Constructor
     public Car(String make,int year, String color, double price) {
         this.make = make;
         this.year = year;
         this.color = color;
         this.price = price;
-
     }
+        //Copy Constructor 
     public Car (Car a){
         this.make = a.make;
         this.year = a.year;
         this.color = a.color;
         this.price = a.price;
     }
-}
 
-        
+
+
+        }
+
+
+        }
+
 
 
